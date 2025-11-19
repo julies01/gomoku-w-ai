75a3851c6d6ffe6d7b330d3958d1d5f8678caba9
julies01 <anjulie.be@gmail.com>
2025-11-19 13:13:15 +0100

    added recap

--- ai-algorithm/julie-t-and-e/AI.py
+++ ai-algorithm/julie-t-and-e/AI.py
@@ -31,20 +31,32 @@ class AI :
             l = ""
             for j in range(board.size):
                 t = self.black_around[i][j]
-                if t == 0:
-                    l += ". "
-                else :  
-                    l += str(self.black_around[i][j]) + " "
+                if t == '●':
+                    l += " " + t + " "
+                elif int(t) > 0 :
+                    l += " " + str(t) + " "
+                elif t == 0 :
+                    l += " . "
+                elif int(t) > 9:
+                    l += " " + str(t)
+                elif int(t) > 99 :
+                    l += str(t)
             print(l)
         print("\nWhite around table : \n")
         for i in range(board.size):
             l = ""
             for j in range(board.size):
                 t = self.white_around[i][j]
-                if t == 0:
-                    l += ". "
-                else :
-                    l += str(self.white_around[i][j]) + " "
+                if t == '◯':
+                    l += " " + t + " "
+                elif t == 0:
+                    l += " . "
+                elif int(t) > 0:
+                    l += " " + str(t) + " "
+                elif int(t) > 9:
+                    l += " " + str(t)
+                elif int(t) > 99 :
+                    l += str(t)
             print(l)
 
     """
@@ -54,6 +66,8 @@ class AI :
     """
 
     def update_white_around (self, board : Board, row : int, col : int) :
+        dict0 = {1 : 110, 2 : 25, 3 : 5, 4 : 1}
+        dict = {1 : 1, 2 : 1, 3 : 1, 4 : 1}
         self.white_around[row][col] = '◯'
         directions = [(1,0), (0,1), (1,1), (-1,1)]
         for vertical, horizontal in directions:
@@ -62,12 +76,12 @@ class AI :
                 new_y = col + i * horizontal
                 if (0 <= new_x < board.size) and (0 <= new_y < board.size):
                      if board.grid[new_x][new_y].state == None:
-                          self.white_around[new_x][new_y] += 1
+                          self.white_around[new_x][new_y] += dict[i]
                 new_x = row - i * vertical
                 new_y = col - i * horizontal
                 if (0 <= new_x < board.size) and (0 <= new_y < board.size):
                      if board.grid[new_x][new_y].state == None:
-                          self.white_around[new_x][new_y] += 1
+                          self.white_around[new_x][new_y] += dict[i]
 
     """
         Updates the black stones adjacency table based on a new stone placement.
@@ -76,6 +90,8 @@ class AI :
     """
 
     def update_black_around (self, board : Board, row : int, col : int) :
+        dict0 = {1 : 110, 2 : 25, 3 : 5, 4 : 1}
+        dict = {1 : 1, 2 : 1, 3 : 1, 4 : 1}
         self.black_around[row][col] = '●'
         directions = [(1,0), (0,1), (1,1), (-1,1)]
         for vertical, horizontal in directions:
@@ -84,10 +100,11 @@ class AI :
                 new_y = col + i * horizontal
                 if (0 <= new_x < board.size) and (0 <= new_y < board.size):
                      if board.grid[new_x][new_y].state == None:
-                          self.black_around[new_x][new_y] += 1
+                          self.black_around[new_x][new_y] += dict[i]
                 new_x = row - i * vertical
                 new_y = col - i * horizontal
                 if (0 <= new_x < board.size) and (0 <= new_y < board.size):
                      if board.grid[new_x][new_y].state == None:
-                          self.black_around[new_x][new_y] += 1
+                          self.black_around[new_x][new_y] += dict[i]
+
 
